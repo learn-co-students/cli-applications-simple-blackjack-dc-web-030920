605767f2d06e5c0362dbe9e79208db88ed9c2379
Deijah Price <deijahwilson1@gmail.com>
2020-03-06 03:59:35 +0000

    Done.

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,44 +1,76 @@
 def welcome
   # code #welcome here
+  puts "Welcome to the Blackjack Table"
 end
 
 def deal_card
   # code #deal_card here
+  rand(1..11)
 end
 
-def display_card_total
+def display_card_total(card_total)
   # code #display_card_total here
+   puts "Your cards add up to #{card_total}"
 end
 
 def prompt_user
-  # code #prompt_user here
+ puts "Type 'h' to hit or 's' to stay"
 end
 
 def get_user_input
   # code #get_user_input here
+  gets.chomp.strip
 end
 
-def end_game
+def end_game(number)
   # code #end_game here
+  puts "Sorry, you hit #{number}. Thanks for playing!"
 end
 
 def initial_round
-  # code #initial_round here
+  card_one = deal_card
+  card_two = deal_card
+  card_total = card_one + card_two
+  display_card_total(card_total) #once you have figured out the sum of your two cards, you can pass the total in as an argumnt so your phrase is displayed.
+  return card_total
 end
 
-def hit?
-  # code hit? here
+def hit?(current_card_value)
+  prompt_user
+  user_input = get_user_input
+  while user_input != "h" && user_input != "s"
+    invalid_command
+    prompt_user
+    user_input = get_user_input
+  end
+  if user_input == "h"
+    current_card_value += deal_card
+  end
+  return current_card_value
 end
 
 def invalid_command
   # code invalid_command here
+  
+  puts "Please enter a valid command"
+  prompt_user
 end
 
+def invalid_command
+  # code invalid_command here
+  puts "Please enter a valid command"
+end
 #####################################################
 # get every test to pass before coding runner below #
 #####################################################
 
 def runner
-  # code runner here
-end
-    
+  welcome
+  total = initial_round
+  until total > 21
+    total = hit?(total)
+  display_card_total(total)
+  end
+  end_game(total)
+
+end	
